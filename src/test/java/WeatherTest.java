2da2a2a2b8dc9009453753edd554371bebdb549d
Iryna Gnatyshko <gnatyshko.i@gmail.com>
2019-05-19 13:04:41 +0200

    HW#2

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -2,22 +2,39 @@ import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
+
 public class WeatherTest {
 
     @Test
     public void getWeatherPerCityTest() {
         RestAssured.baseURI = "https://pinformer.sinoptik.ua/search.php";
-        ValidatableResponse response= RestAssured.given()
+        ValidatableResponse response = RestAssured.given()
                 .param("lang", "ua")
                 .param("return_id", 1)
                 .param("q", "Lviv")
                 //.log().uri()
                 .get()
                 .then()
-               //.log().all()
+                //.log().all()
                 .statusCode(200);
 
-        String cityId=response.extract().asString();
+        String cityId = response.extract().asString();
         System.out.println(cityId);
+
+        System.out.println(cityId.substring(47));
+        RestAssured.baseURI = "https://pinformer.sinoptik.ua/pinformer4.php";
+
+        ValidatableResponse responseWeatherInCity = RestAssured.given()
+                .param("lang", "ua")
+                .param("id", 303014487)
+                .log().uri()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
+        String cityInfoWeather = responseWeatherInCity.extract().asString();
+        System.out.println(responseWeatherInCity);
+
+
     }
 }
