6b799eeb474bf4099c31a5e568f23dad9f243f1d
Iryna Gnatyshko <gnatyshko.i@gmail.com>
2019-05-23 12:14:03 +0200

    CW#4

--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -2,39 +2,47 @@ import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+
 
 public class WeatherTest {
 
     @Test
     public void getWeatherPerCityTest() {
-        RestAssured.baseURI = "https://pinformer.sinoptik.ua/search.php";
+        String cityName="Lviv";
+        RestAssured.baseURI = "https://pinformer.sinoptik.ua/";
+        // RestAssured.basePath= "search.php";
         ValidatableResponse response = RestAssured.given()
+                .basePath("search.php")
                 .param("lang", "ua")
                 .param("return_id", 1)
-                .param("q", "Lviv")
+                .param("q", cityName)
                 //.log().uri()
                 .get()
                 .then()
                 //.log().all()
                 .statusCode(200);
 
-        String cityId = response.extract().asString();
+        String responseString = response.extract().asString();
+        String cityId = responseString.substring(responseString.lastIndexOf("|") + 1);
         System.out.println(cityId);
 
-        System.out.println(cityId.substring(47));
-        RestAssured.baseURI = "https://pinformer.sinoptik.ua/pinformer4.php";
+        //System.out.println(cityId.substring(47));
+        // RestAssured.basePath= "pinformer4.php";
 
-        ValidatableResponse responseWeatherInCity = RestAssured.given()
-                .param("lang", "ua")
-                .param("id", 303014487)
+        ValidatableResponse response1 = RestAssured.given()
+                .param("type", "js")
+                .param("lang", "en")
+                .param("id", cityId)
                 .log().uri()
-                .get()
+                .get("pinformer4.php")
                 .then()
                 .log().all()
-                .statusCode(200);
-        String cityInfoWeather = responseWeatherInCity.extract().asString();
-        System.out.println(responseWeatherInCity);
-
+                .statusCode(200)
+                .body("any {it.key=='{pcity}' }", is(true))//Grrovy path with hamcrest matchers
+                .body("'{pcity}'", is(not(1)));//JSON path with hamcrest matchers
+        System.out.println(response1.extract().path("'{pcity}'"));
 
     }
 }
